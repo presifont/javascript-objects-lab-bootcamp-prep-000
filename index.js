edb18bce316e6442411641c7dd78c1ae85c216c5
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-22 01:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return recipes.cake = { prop: 1, prop2: 2 }
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({ prop: 1, prop2: 2 })
+  Object.assign({ prop: 1, prop2: 2 })
+  
 }
