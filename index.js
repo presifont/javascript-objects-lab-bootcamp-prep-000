95f4795326a809afce99e42c19b5b38da6d6a411
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-22 03:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,7 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, obj)
     return newObj
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
